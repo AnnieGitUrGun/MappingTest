11bec007758702269b504ece55e09e54fa8f60fa
Anne Canoune <A.Canoune@gmail.com>
2013-06-25 17:17:41 -0400

    added point layer for further testing

---
+++ AnnesPOI.js
@@ -0,0 +1,20 @@
+{
+"type": "FeatureCollection",
+                                                                                
+"features": [
+{ "type": "Feature", "id": 0, "properties": { "id": 1, "Name": "My House", "City": "White Hall", "Comments": "It's OK" }, "geometry": { "type": "Point", "coordinates": [ -76.596162208688156, 39.684531104951148 ] } }
+,
+{ "type": "Feature", "id": 1, "properties": { "id": 2, "Name": "Gan's House", "City": "Maryland Line", "Comments": "Loved this place!" }, "geometry": { "type": "Point", "coordinates": [ -76.653723913935096, 39.717830479045617 ] } }
+,
+{ "type": "Feature", "id": 2, "properties": { "id": 3, "Name": "Seventh District Elementary", "City": "Parkton", "Comments": "Didn't like this place" }, "geometry": { "type": "Point", "coordinates": [ -76.646548275845319, 39.681134369439647 ] } }
+,
+{ "type": "Feature", "id": 3, "properties": { "id": 4, "Name": "St. James' Episcopal Church", "City": "Parkton", "Comments": "My home church" }, "geometry": { "type": "Point", "coordinates": [ -76.654631746220502, 39.651267632314138 ] } }
+,
+{ "type": "Feature", "id": 4, "properties": { "id": 5, "Name": "Hereford High School", "City": "Parkton", "Comments": "Made it out alive!" }, "geometry": { "type": "Point", "coordinates": [ -76.659599735199748, 39.599004022538239 ] } }
+,
+{ "type": "Feature", "id": 5, "properties": { "id": 6, "Name": "Hereford Middle School", "City": "Sparks", "Comments": "Worst years of my life, thus far." }, "geometry": { "type": "Point", "coordinates": [ -76.653166524446036, 39.572674275660262 ] } }
+,
+{ "type": "Feature", "id": 6, "properties": { "id": 7, "Name": "U. Maryland Baltimore County", "City": "Baltimore", "Comments": "B.S. Environmental Science" }, "geometry": { "type": "Point", "coordinates": [ -76.713371083173911, 39.253044859950535 ] } }
+
+]
+}
